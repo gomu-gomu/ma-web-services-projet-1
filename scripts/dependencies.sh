cc96520eea973a8c661e323d28c8787c0db3eb8c
EOussama <oussama_gtacj@live.fr>
2024-01-10 23:47:15 +0100

    Dependencies

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,5 +3,5 @@
 mkdir -p ./src/verbose/bootstrap
 mkdir -p ./src/verbose/particles.js
 
-cp -R ./node_modules/bootstrap/dist ./src/verbose/bootstrap
+cp -R ./node_modules/bootstrap/dist/* ./src/verbose/bootstrap
 cp ./node_modules/particles.js/particles.js ./src/verbose/particles.js/particles.js
