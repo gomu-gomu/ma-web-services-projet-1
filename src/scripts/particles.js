3664bd76d577612965ef91b3ca621d0af05a4934
EOussama <oussama_gtacj@live.fr>
2024-01-10 23:47:22 +0100

    Particles

---
+++ src/scripts/particles.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  particlesJS.load('particles', 'config/particles.json', function () {
+    console.log('callback - particles-js config loaded');
+  });
+});
