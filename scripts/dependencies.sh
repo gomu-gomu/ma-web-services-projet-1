cf0edca2e0761d7a90fc5302b1db8099a95ea3d7
EOussama <oussama_gtacj@live.fr>
2024-01-10 23:29:59 +0100

    Particles.js

---
+++ scripts/dependencies.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+mkdir -p ./src/verbose/bootstrap
+mkdir -p ./src/verbose/particles.js
+
+cp -R ./node_modules/bootstrap/dist ./src/verbose/bootstrap
+cp ./node_modules/particles.js/particles.js ./src/verbose/particles.js/particles.js
