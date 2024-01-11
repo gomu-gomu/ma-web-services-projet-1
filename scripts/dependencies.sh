51f442940e07c1b8831e95dd5eefdaa307f985f3
EOussama <oussama_gtacj@live.fr>
2024-01-11 13:17:26 +0100

    CounUp.js

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,10 +3,12 @@
 mkdir -p ./src/verbose/chart.js
 mkdir -p ./src/verbose/bootstrap
 mkdir -p ./src/verbose/worldatlas
+mkdir -p ./src/verbose/countup.js
 mkdir -p ./src/verbose/particles.js
 
 cp -R ./node_modules/bootstrap/dist/* ./src/verbose/bootstrap
 cp -R ./node_modules/chart.js/dist/chart.umd.js ./src/verbose/chart.js/chart.umd.js
 cp ./node_modules/particles.js/particles.js ./src/verbose/particles.js/particles.js
+cp ./node_modules/countup.js/dist/countUp.umd.js ./src/verbose/countup.js/countUp.umd.js
 cp ./node_modules/world-atlas/countries-50m.json ./src/verbose/worldatlas/countries-50m.json
 cp ./node_modules/chartjs-chart-geo/build/index.umd.min.js ./src/verbose/chart.js/index.umd.min.js
