b8fe3448c9791318f5adc4cb6a7256f5ee221a74
EOussama <EOussama@users.noreply.github.com>
2024-01-11 12:24:47 +0000

    deploy: 5064d3d21a069e611f20c5cfe9581d389bd7e432

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
+  fetch('https://disease.sh/v3/covid-19/all')
+    .then(e => e.json())
+    .then(e => ({
+      cases: e.cases,
+      deaths: e.deaths,
+      recovered: e.recovered
+    }))
+    .then(data => {
+      const keys = Object.keys(data);
 
+      for (const key of keys) {
+        const number = data[key];
+        const counter = new countUp.CountUp(`stat-${key}`, number);
+
+        counter.start();
+      }
+    });
 });
