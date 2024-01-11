5e38fff4419ceecbe2858773457ca713de706176
EOussama <oussama_gtacj@live.fr>
2024-01-11 12:03:33 +0100

    Chart map geo

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,8 +2,11 @@
 
 mkdir -p ./src/verbose/chart.js
 mkdir -p ./src/verbose/bootstrap
+mkdir -p ./src/verbose/worldatlas
 mkdir -p ./src/verbose/particles.js
 
 cp -R ./node_modules/bootstrap/dist/* ./src/verbose/bootstrap
 cp -R ./node_modules/chart.js/dist/chart.umd.js ./src/verbose/chart.js/chart.umd.js
 cp ./node_modules/particles.js/particles.js ./src/verbose/particles.js/particles.js
+cp ./node_modules/world-atlas/countries-50m.json ./src/verbose/worldatlas/countries-50m.json
+cp ./node_modules/chartjs-chart-geo/build/index.umd.min.js ./src/verbose/chart.js/index.umd.min.js
