5064d3d21a069e611f20c5cfe9581d389bd7e432
EOussama <oussama_gtacj@live.fr>
2024-01-11 13:23:18 +0100

    Cards API

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
-  fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=all')
+  fetch('https://disease.sh/v3/covid-19/all')
     .then(e => e.json())
+    .then(e => ({
+      cases: e.cases,
+      deaths: e.deaths,
+      recovered: e.recovered
+    }))
     .then(data => {
       const keys = Object.keys(data);
 
       for (const key of keys) {
-        const number = getLastRecordedStat(data, key);
+        const number = data[key];
         const counter = new countUp.CountUp(`stat-${key}`, number);
 
         counter.start();
       }
     });
-
-  function getLastRecordedStat(data, key) {
-    const stat = Object
-      .entries(data[key])
-      .reverse()
-      .find(e => e[1] > 0);
-
-    return stat[1] || 0;
-  }
 });
