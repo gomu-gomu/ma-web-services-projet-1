0317c6a75796212160b050ca78606249953ce722
EOussama <oussama_gtacj@live.fr>
2024-01-11 10:34:49 +0100

    Refactor

--- src/scripts/particles.js
+++ src/scripts/particles.js
@@ -1,5 +1,3 @@
 window.addEventListener('DOMContentLoaded', () => {
-  particlesJS.load('particles', 'config/particles.json', function () {
-    console.log('callback - particles-js config loaded');
-  });
+  particlesJS.load('particles', 'config/particles.json');
 });
