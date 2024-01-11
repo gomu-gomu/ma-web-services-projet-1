8e4e050222456674a97eb83eb523cc584e2e4c6a
EOussama <oussama_gtacj@live.fr>
2024-01-11 10:34:36 +0100

    Chart.js

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
+mkdir -p ./src/verbose/chart.js
 mkdir -p ./src/verbose/bootstrap
 mkdir -p ./src/verbose/particles.js
 
 cp -R ./node_modules/bootstrap/dist/* ./src/verbose/bootstrap
+cp -R ./node_modules/chart.js/dist/chart.umd.js ./src/verbose/chart.js/chart.umd.js
 cp ./node_modules/particles.js/particles.js ./src/verbose/particles.js/particles.js
