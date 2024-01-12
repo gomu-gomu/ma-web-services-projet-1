a9e2860fd4b35d7d46d1c6401af9e3712a33d359
EOussama <oussama_gtacj@live.fr>
2024-01-12 17:45:16 +0100

    Loader

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,18 @@
 import { sendReadyEvent } from './utils.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
+  const modalElement = document.getElementById('loader-modal');
+  const options = { keyboard: false, backdrop: false };
+  const modal = new bootstrap.Modal(modalElement, options);
+
   let eventsRecorded = 0;
+  modal.show();
 
   document.addEventListener('apiLoaded', (e) => {
     ++eventsRecorded;
 
     if (eventsRecorded === 4) {
+      modal.hide();
       sendReadyEvent();
     }
   });
